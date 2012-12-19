c7ac8dce1ecdf8ca368078f095f04b6db9ef7570
WilliamHuey <lliw1236@yahoo.com>
2012-12-18 18:55:38 -0800

    install stylus and nib

--- app.js
+++ app.js
@@ -7,14 +7,25 @@ var express = require('express')
   , routes = require('./routes')
   //, user = require('./routes/user')
   , http = require('http')
+  , stylus = require('stylus')
+  , nib = require('nib')
   , path = require('path');
 
 var app = express();
+
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .use(nib())
+        .import('nib');
+}
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(stylus.middleware({src: __dirname + '/public', compile : compile}));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
