7f92711f0c1e24b0f94b8b3cee50fa5277d2b1ad
WilliamHuey <lliw1236@yahoo.com>
2012-12-21 20:43:21 -0800

    change the asset path to use assets instead of using public

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(stylus.middleware({src: __dirname + '/assets', compile : compile}));
-  app.use(connectAssets());
+  app.use(connectAssets({src: __dirname + "/assets/"}));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'assets')));
 });
 
 app.configure('development', function(){
